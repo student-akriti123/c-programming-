c8a5b528e8a907d4c171e3436697373c782051e2
student-akriti123 <akritisinha1602@gmail.com>
2023-11-28 04:44:30 +0530

    first commit

---
+++ variable.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+
+ int a;
+ printf("Enter the value a\n");
+ scanf("%d",&a);
+
+   int b;
+ printf("Enter the value b\n");
+ scanf("%d",&b);
+
+   int c;
+ printf("Enter the value c\n");
+ scanf("%d",&c);
+
+        printf("The sum of a,b and c is %d\n",a+b+c);
+        printf("The difference of a and b is %d\n",a-b);
+        printf("The multiple of a,b and c is %d\n",a*b*c);
+        printf("The division of a and b is %d\n",a/b);
+
+    return 0;
+}
