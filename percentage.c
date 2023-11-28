7c747ef37eb13a291eb6a958405ab3806f46f445
student-akriti123 <akritisinha1602@gmail.com>
2023-11-28 11:14:31 +0530

    first commit

---
+++ percentage.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+ float m1 = 76;
+ float m2 = 54;
+ float m3 = 89;
+ float m4 = 99;
+ float m5 = 86;
+
+ float percentage = (m1+m2+m3+m4+m5)/5;
+
+   printf("The percentage is %f",percentage);
+
+return 0;
+}
