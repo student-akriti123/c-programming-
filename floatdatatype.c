11f985408334165f43358412fc7a2aae38307aa6
student-akriti123 <akritisinha1602@gmail.com>
2023-11-28 05:43:08 +0530

    first commit

---
+++ floatdatatype.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  float a;
+ printf("Enter the value a\n");
+ scanf("%d",&a);
+
+   float b;
+ printf("Enter the value b\n");
+ scanf("%d",&b);
+
+  float c = a/b;
+  printf("%f",c);
+
+return 0;
+}
